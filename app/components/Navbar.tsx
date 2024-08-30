9934deab4a24fd202deeae4bf8ea5fed62b2eb94
Jovany Cardoza-Aguilar <jovanycardozaaguilar@gmail.com>
2024-08-29 17:06:58 -0700

    Change navbar text size

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,22 +6,22 @@ const Navbar = () => {
         <div className='bg-gray-900 h-20 w-full border-b-2 flex items-center justify-center p-2'>
             <ul className='flex'>
                 <li className='p-4 cursor-pointer'>
-                    <Link href = '/' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+                    <Link href = '/' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
                     Home
                     </Link>
                 </li>
                 <li className='p-4 cursor-pointer'>
-                    <Link href = '/projects' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+                    <Link href = '/projects' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
                     Projects
                     </Link>
                 </li>
                 <li className='p-4 cursor-pointer'>
-                    <Link href = '/publications' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+                    <Link href = '/publications' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
                     Publications
                     </Link>
                 </li>
                 <li className='p-4 cursor-pointer'>
-                    <Link href = '/resume' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+                    <Link href = '/resume' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
                     Resume
                     </Link>
                 </li>
