29e17e96d6bcc1db24406631cf3e7186f8e73f03
Jovany Cardoza-Aguilar <jovanycardozaaguilar@gmail.com>
2024-08-29 17:27:21 -0700

    Fixed pdf mobile not showing

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,26 +3,26 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='bg-gray-900 h-20 w-full border-b-2 flex items-center justify-center p-2'>
-            <ul className='flex'>
-                <li className='p-4 cursor-pointer'>
-                    <Link href = '/' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
-                    Home
+        <div className='bg-gray-900 h-20 w-full border-b-2 flex items-center justify-center md:p-4'>
+            <ul className='flex flex-wrap justify-center space-x-4'>
+                <li className='md:p-4'>
+                    <Link href='/' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+                        Home
                     </Link>
                 </li>
-                <li className='p-4 cursor-pointer'>
-                    <Link href = '/projects' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
-                    Projects
+                <li className='md:p-4'>
+                    <Link href='/projects' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+                        Projects
                     </Link>
                 </li>
-                <li className='p-4 cursor-pointer'>
-                    <Link href = '/publications' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
-                    Publications
+                <li className='md:p-4'>
+                    <Link href='/publications' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+                        Publications
                     </Link>
                 </li>
-                <li className='p-4 cursor-pointer'>
-                    <Link href = '/resume' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
-                    Resume
+                <li className='md:p-4'>
+                    <Link href='/resume' className='text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+                        Resume
                     </Link>
                 </li>
             </ul>
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div className='p-4'>
+        <div className='p-4 p-4 md:p-6 lg:p-8'>
             <div className='mt-4 flex justify-center'>
-                <embed src='Res-2024.pdf' width="700px" height="700px" />
+                <iframe className="pdf w-full max-w-4xl h-[500px] sm:h-[600px] lg:h-[700px] xl:h-[800px]" 
+                    src= "Res-2024.pdf"
+                    >
+                </iframe>
             </div>
         </div>
         
